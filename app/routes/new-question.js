fd3d4158ce99278feff9eeadbe81b58e69c79588
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-11 10:46:12 -0700

    refactor new question routes to include model for edit save functionality

--- app/routes/new-question.js
+++ app/routes/new-question.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+
   actions: {
     saveQuestion(params) {
     var newQuestion = this.store.createRecord('question', params);
@@ -15,12 +19,12 @@ export default Ember.Route.extend({
     }
     });
     question.save();
-    this.transitionTo('index');
+    this.transitionTo('new-question');
     },
 
     destroyQuestion(question) {
      question.destroyRecord();
-     this.transitionTo('index');
+     this.transitionTo('new-question');
     }
     }
     });
