b805a04f1f40f42c9bfacfd0c976f90ff261d566
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-11 14:24:33 -0700

    add functioning delete btn for answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -7,13 +7,22 @@ export default Ember.Route.extend({
 
   actions: {
     saveAnswer(params) {
-       var newAnswer = this.store.createRecord('answer', params);
-       var question = params.question;
-       question.get('answers').addObject(newAnswer);
-       newAnswer.save().then(function() {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
          return question.save();
        });
-       this.transitionTo('question', question)
+      this.transitionTo('question', question)
+     },
+
+    delete(question) {
+      if (confirm('Are you sure you want to delete this answer?')) {
+         question.destroyRecord();
+         this.transitionTo('question');
+         }
+       }
      }
-}
+
+
 });
